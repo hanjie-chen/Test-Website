586192bd3a415767cdcd6679713341c10c540080
Plain <1228200516@qq.com>
2024-10-31 14:13:17 +0000

    add some code

--- import_artciles_scripts.py
+++ import_artciles_scripts.py
@@ -1,6 +1,11 @@
 from app import app
+import os
 
 Articles_Directory = "/home/Plain/Personal_Project/Test_Articles_Data"
 
 def import_articles():
-    pass
+    # 获取所有.md files
+    md_files = [f for f in os.listdir(Articles_Directory) if f.endswith('.md')]
+
+    for md_files in md_files:
+        pass
